6512c0f381dc98d222ff6bcaab230a3b0cc8965c
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-06-07 19:40:22 +0430

    Kotlin_divar

---
+++ app/src/main/java/Models/Ads.kt
@@ -0,0 +1,15 @@
+package Models
+
+class Ads(
+    var id: Int,
+    var title: String,
+    var description: String,
+    var city: String,
+    var date: String,
+    var phone: String,
+    var option: String,
+    var price: Int,
+    var cat: String,
+
+    var url: String//عکس
+) 
