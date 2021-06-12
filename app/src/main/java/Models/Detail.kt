f1df6ac043a3b5d03493d0279bef7668b42588da
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2021-06-12 10:27:56 +0430

    Kotlin_divar

---
+++ app/src/main/java/Models/Detail.kt
@@ -0,0 +1,15 @@
+package Models
+
+class Detail(
+    var id: String,
+    var title: String,
+    var description: String,
+    var city: String,
+    var date: String,
+    var phone: String,
+    var price: String,
+    var option: String,
+    var url: List<String>,
+    var cat:String
+) {
+}
